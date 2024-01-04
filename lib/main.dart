dd296497610d79b1dc4997e1a1639894ea0f2c0f
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-01-04 17:44:06 +0530

    Scaffold Magic

--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,21 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
           title: const Text('My First App!'),
         ),
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            const Align(
-              alignment: Alignment.center,
-              child: Icon(Icons.verified),
-            ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            print('You clicked me!');
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: 'Business'),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
           ],
         ),
+        drawer: const Drawer(child: Text("Sup!")),
       ),
     );
   }
